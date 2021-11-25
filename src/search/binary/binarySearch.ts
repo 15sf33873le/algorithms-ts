3481b8fcb9fa9e53b9d12a0f4b8d76f85a281437
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-25 09:49:55 +0000

    again

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,8 +1,41 @@
 import { NO_MATCH } from "../common"
+let result: number;
 
 const binarySearch = (items: any[], item: any): number => {
 
-    return NO_MATCH;
+
+    // Calculate initial values of lowest/highest pointers to encapsulate whole list
+    let lowest_ptr: number = 0
+    let highest_ptr: number = items.length - 1
+
+    while (!result) {
+        // Calculate new middle and make the comparison
+        let middle_ptr: number = Math.floor((lowest_ptr + highest_ptr) / 2)
+        console.log(middle_ptr);
+        if (item == items[middle_ptr]){
+            // Item found, return its index
+            result = middle_ptr
+        }
+        else if (lowest_ptr == highest_ptr){
+            // This is the exit condition, we haven't found what we are looking for
+            result = NO_MATCH
+        }
+        else if (item < items[middle_ptr]){
+            // If the item is smaller than the middle, look in the lower half
+            if (middle_ptr == 0){
+                result = NO_MATCH
+            }
+            highest_ptr = middle_ptr - 1
+        }
+        else if (item > items[middle_ptr]){
+            // The item is larger than the middle, look in the upper half
+            if (middle_ptr == items.length - 1){
+                result = NO_MATCH
+            lowest_ptr = middle_ptr + 1
+            }
+        }
+    }
+    return result
 }
 
 export default binarySearch;
--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -2,6 +2,13 @@ import { NO_MATCH } from "../common";
 
 const linearSearch = (items: any[], item: any): number => {
 
+    let i = items.length
+
+    for (i>0; i--;) {
+        if (items[i] == item) {
+            return i;
+        }
+    }
     return NO_MATCH;
 }
 
