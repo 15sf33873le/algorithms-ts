139cba66c7a528543f2288b31a6830784de22a45
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-12-09 09:17:18 +0000

    updated code

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,46 +1,41 @@
-import { NO_MATCH } from "../common"
-let result: number;
-
 const binarySearch = (items: any[], item: any): number => {
 
 
     // Calculate initial values of lowest/highest pointers to encapsulate whole list
     let lowest_ptr: number = 0
     let highest_ptr: number = items.length - 1
-    let done = false
+    let final: number = -2
+    let middle_ptr: number = -2
 
-    while (done != true) {
+    do {
         // Calculate new middle and make the comparison
-        let middle_ptr: number = Math.floor((lowest_ptr + highest_ptr) / 2)
+        middle_ptr = Math.floor((lowest_ptr + highest_ptr) / 2)
         console.log(middle_ptr);
         if (item == items[middle_ptr]){
             // Item found, return its index
-            result = middle_ptr
-            done = true
+            final = middle_ptr
         }
         else if (lowest_ptr == highest_ptr){
             // This is the exit condition, we haven't found what we are looking for
-            result = NO_MATCH
-            done = true
+            final = -1
         }
         else if (item < items[middle_ptr]){
             // If the item is smaller than the middle, look in the lower half
             if (middle_ptr == 0){
-                result = NO_MATCH
-                done = true
+                final = -1
             }
             highest_ptr = middle_ptr - 1;
         }
         else if (item > items[middle_ptr]){
             // The item is larger than the middle, look in the upper half
             if (middle_ptr == items.length - 1){
-                result = NO_MATCH
-                done = true
-            lowest_ptr = middle_ptr + 1;
+                final = -1
             }
+            lowest_ptr = middle_ptr + 1;    
         }
     }
-    return result
+    while (final == -2);
+    return final
 }
 
 export default binarySearch;
--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -4,7 +4,14 @@ import swap from "../swap";
 const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
     // Your code here
-
+    let lim = items.length
+    for(let i=0; i<lim; i++) {
+        for (let j=0 ;j < lim-i; j++) {
+            if (items[j] > items[j+1]) {
+                swap(items,j,j+1);
+            }
+        }
+    }
     return items;
 }
 
